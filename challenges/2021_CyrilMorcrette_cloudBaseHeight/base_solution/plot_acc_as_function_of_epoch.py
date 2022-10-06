9d9ad62257b50ac8be22f4c1cb85ef12c5e3cdf9
Harvey South <harvey.south@metoffice.gov.uk>
2022-10-06 14:46:18 +0100

    Update plot function to work with rest of base solution

--- challenges/2021_CyrilMorcrette_cloudBaseHeight/base_solution/plot_acc_as_function_of_epoch.py
+++ challenges/2021_CyrilMorcrette_cloudBaseHeight/base_solution/plot_acc_as_function_of_epoch.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python	
-#
 # Cyril Morcrette (2020), Met Office, UK
 #
 # Use "module load scitools/experimental-current" at the command line before running this.
@@ -8,26 +6,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filein='cbh_ml_timeseries_rmse_train.txt'
+filein='cbh_ml_timeseries_acc_train.txt'
 train=np.loadtxt(filein)
 
-filein='cbh_ml_timeseries_rmse_val.txt'
-val=np.loadtxt(filein)
-
+with open('cbh_ml_timeseries_acc_val.txt') as f:
+        val=np.loadtxt(f)
 tmp=train.shape
 nt=tmp[0]
 
 train=train[1:nt]
 val=val[1:nt]
 
-# Since there are 124 files to read in before an epoch is completed
-# and since the cbh_ml_timeseries_rmse_*.txt files are updated after training on each file
-# one needs to divide by 124 to get a correct epoch counter.
 time=np.arange(1,nt)/124.0
-
 plt.plot(time,train,'k+-')
 plt.xlabel('Epochs')
 plt.ylabel('RMSE')
 plt.plot(time,val,'b+-')
-fig.savefig('cbh_ml_rmse_plot.png')
-
+plt.show()
+plt.figure().savefig('cbh_ml_acc_plot.png')
