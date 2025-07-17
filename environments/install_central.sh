3abc5035bf6ac96034ecb1183cd40354aad505f0
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2025-07-17 17:10:51 +0100

    fixed some bugs in install script.

--- environments/install_central.sh
+++ environments/install_central.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-ENV_LIST="xgboost pytorch sklearn tensorflow"
+ENV_LIST="xgboost pytorch sklearn tensorflow tensorflow-gnn"
 
 for env_name in ${ENV_LIST}; do
     echo creating ${env_name} environment
-    conda env create --file environments/requiremments_${env_name}.yml
+    conda env create --file environments/requirements_${env_name}.yml 
+done
