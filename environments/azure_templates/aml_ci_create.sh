36af9c961271e09715df03d059f18550ad50f4c4
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2026-08-04 11:27:45 +0100

    updated the compute setup script

--- environments/azure_templates/aml_ci_create.sh
+++ environments/azure_templates/aml_ci_create.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-mkdir ~/data
-cd ~/data
+export DATA_ROOT_DIR=~/data
+
+mkdir $DATA_ROOT_DIR
+cd $DATA_ROOT_DIR
 wget https://zenodo.org/records/7390654/files/xbt_1968.csv
-
-
+wget https://zenodo.org/records/21773439/files/climate_zones_1p0.csv
 
 mkdir ~/downloads/
 # cd ~/downloads
 # bash ./Miniconda3-latest-Linux-x86_64.sh
 
-mkdir ~/prog/
-cd ~/prog/
+export PROG_ROOT_DIR=~/prog
+mkdir $PROG_ROOT_DIR
 
-git clone https://github.com/informatics-lab/ml_weather_tutorial/ 
-cd ml_weather_tutorial
+git clone https://github.com/informatics-lab/ml_weather_tutorial/ $PROG_ROOT_DIR/ml_weather_tutorial
+git clone https://github.com/MetOffice/data_science_cop.git $PROG_ROOT_DIR/data_science_cop
+
+# this is temporary until we can get the azure_pathways branch merged into main
+cd $PROG_ROOT_DIR/data_science_cop
+git switch azure_pathways
+
 
 
