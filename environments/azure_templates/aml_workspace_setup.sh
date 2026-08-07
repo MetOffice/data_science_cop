b3a6b230905307bfa8df1ce6604b9d81046e3134
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2026-08-07 09:25:02 +0100

    updates for creating an environment.

--- environments/azure_templates/aml_workspace_setup.sh
+++ environments/azure_templates/aml_workspace_setup.sh
@@ -14,14 +14,14 @@ export PLATFORM=metoffice
 
 
 export WORKSPACE_NAME=dscoptest1
-export RESOURCE_GROUP=1-1a0e0d26-playground-sandbox
+export RESOURCE_GROUP=$(python get_resource_groups.py)
 
 # this must be updated by the user before running
-export USER_NAME=cloud_user_p_dcc359dd/
+export USER_NAME=cloud_user_p_540617ae
 #===============================
 # Create workspace
 
-python create_workspace_spec.py --name $WORKSPACE_NAME --resource-group $RESOURCE_GROUP
+python create_workspace_spec.py workspace --name $WORKSPACE_NAME --resource-group $RESOURCE_GROUP --description "Workspace for DS CoP AzureML tutorials." --output workspace_spec.yaml
 az ml workspace create --file workspace_spec.yaml --resource-group $RESOURCE_GROUP
 
 
@@ -77,57 +77,10 @@ az ml compute create --name dscopcitest01 --size $COMPUTE_SIZE --type ComputeIns
 az ml compute create --name dscopclustertest01 --size $COMPUTE_SIZE --min-instances 0 --max-instances 1 --type AmlCompute --resource-group $RESOURCE_GROUP --workspace-name $WORKSPACE_NAME
 
 
-#todo: run command to create file from template
-az ml environment create -f environments/azure_templates/envs/keras/env_keras_aml.yaml -w mlw-dscoptrainingenv-uksouth-01 -g rg-dscop-dscoptrainingenv
+#=========================================
+# Create compute instances and clusters
+python create_workspace_spec.py environment  --conda-file ../requirements_pytorch.yml --name dscop_pytorch --output env_dscop_pytorch_spec.yaml --description "Environment for DS CoP AzureML tutorials with PyTorch and other dependencies." --image  "mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04"
 
-# SECTION - set up datasets
-
-
-#todo wget weatherbench 5 degree snapshot data
-
-# upload to azure blob storage
+az ml environment create --file env_dscop_pytorch_spec.yaml --resource-group $RESOURCE_GROUP --workspace-name $WORKSPACE_NAME
 
 
-az storage account list
-export STORAGE_ACCOUNT=$(python get_storage_account.py $RESOURCE_GROUP --first-only)
-export CONTAINER_NAME=azureml
-export CONTAINER_URL= https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}
-
-export XBT_DATA_PATH=/data/users/dscop/ml_tutorial/xbt/xbt_1968.csv
-azcopy copy ${XBT_DATA_PATH} ${CONTAINER_URL}/xbt/xbt_1968.csv
-
-
-if [ "${PLATFORM}" = "metoffice" ]; then
-  export XBT_DATA_PATH=/data/users/dscop/ml_tutorial/xbt/xbt_1968.csv
-  export CLIMATE_ZONES_DATA_PATH=/data/users/dscop/ml_tutorial/climate_zones/ml_ready/climate_zones_1p0.csv
-else
-  export DATA_PATH=./data
-  export XBT_DATA_PATH="${DATA_PATH}/xbt/xbt_1968.csv"
-  export CLIMATE_ZONES_DATA_PATH="${DATA_PATH}/climate_zones/ml_ready/climate_zones_1p0.csv"
-
-  mkdir -p "$(dirname "${XBT_DATA_PATH}")" "$(dirname "${CLIMATE_ZONES_DATA_PATH}")"
-  wget -O "${XBT_DATA_PATH}" https://zenodo.org/records/7390654/files/xbt_1968.csv
-  wget -O "${CLIMATE_ZONES_DATA_PATH}" https://zenodo.org/records/21773439/files/climate_zones_1p0.csv
-fi
-
-azcopy copy "${XBT_DATA_PATH}" "${CONTAINER_URL}/xbt/xbt_1968.csv"
-azcopy copy "${CLIMATE_ZONES_DATA_PATH}" "${CONTAINER_URL}/climate_zones/climate_zones_1p0.csv"
-
-#download the data
-wget https://zenodo.org/records/7390654/files/xbt_1968.csv
-wget https://zenodo.org/records/21773439/files/climate_zones_1p0.csv
-
-export CLIMATE_ZONES_DATA_PATH=/data/users/dscop/ml_tutorial/climate_zones/ml_ready/climate_zones_1p0.csv
-azcopy copy ${CLIMATE_ZONES_DATA_PATH} https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}/climate_zones/climate_zones_1p0.csv
-
-# todo: copy weatherbench data
-
-
-# create azure datastores
-#todo: create datastores for xbt and climate zones and weatherbench
-#TODO: create datastore yaml file
-az ml datastore create --name <datastore_name> --type azureblob --account-name <storage_account_name> --container-name <container_name> --resource-group $RESOURCE_GROUP --workspace-name $WORKSPACE_NAME
-
-# create azure datasets
-#TODO: create dataset yaml file
-az ml dataset create --name <dataset_name> --type tabular --path <datastore_name>/<path_to_data> --resource-group $RESOURCE_GROUP --workspace-name $WORKSPACE_NAME
