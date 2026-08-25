9e1da3fc1d4e473b139d32a6cb7cb194868d5173
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2026-08-25 16:18:01 +0100

    created a small wrapper for environments to also install the azureml sdk.

---
+++ environments/azure_templates/azure_conda_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This script is a small wrapper for intalling on fo the conda environments for the DS CoP on Azure ML. The wrapper
+# primarily just calls conda create, and then uses pip to install the AzureML SDK and other packages that are
+# not available through conda. It also sets up the kernel for use in Jupyter notebooks.
+
+export CONDA_REQUIREMENTS_FILE=$1
+export CONDA_ENV_NAME=$2
+
+conda env create --file $CONDA_REQUIREMENTS_FILE
+
+conda activate $CONDA_ENV_NAME
+pip install mltable azure-ai-ml azureml-dataprep[pandas]
+
+python -m ipykernel install --user --name $CONDA_ENV_NAME --display-name "$CONDA_ENV_NAME"z
